beb6752391a390724ef60577ea40d13d492feffb
ChrisQuickfall <141458257+ChrisQuickfall@users.noreply.github.com>
2023-08-05 21:50:16 +0100

    Add files via upload
    Comments added to R program to make clearer how to use.
    Lesson slide template uploaded.

--- Weekly Lesson Creation.R
+++ Weekly Lesson Creation.R
@@ -4,21 +4,58 @@ library(lubridate)
 library(stringr)
 library(openxlsx)
 
+### USER INPUTS
+
 # Choose your week reference; this should match what's in the week column in 
 # your timetable file.
+
 week <- 1
 
+# Choose the main folder in which you're working. Make sure this contains subfolders
+# labelled 'Archive', 'Templates', and 'This Week'
+
+active_folder <- "C://Chris//Teaching"
+
+# Choose the name for the archive subfolder in which you want archival lessons
+# to be placed.
+
+archive_folder_name <- "2022-2023"
+
+
+
+### EDITABLE INPUTS
 
 lesson_template_name <- "Lesson Slide Template.pptx"
-lesson_template_folder <- "C://Chris//Teaching//Templates"
-target_folder <- "C://Chris//Teaching//This Week"
-archive_folder <- "C://Chris//Teaching//Archive//2022-2023"
 
 
-# find timetable
+### PROGRAM
+
+# generates folder names from your main folder
+
+target_folder <- paste0(active_folder, "//This Week//")
+archive_folder <- paste0(active_folder, "//Archive//", archive_folder_name, "//")
+lesson_template_file_location <- paste0(active_folder, "//Templates//", lesson_template_name)
+
+
+# import timetable
 timetable <- read.xlsx("C://Chris//Teaching//Timetable.xlsx") %>% 
   rename(Week = 1) %>% 
   filter(Week == week)
+
+
+
+
+# move last week's pptx into archive
+
+for(xxx in unique(timetable[,4])){
+  lw_powerpoints <- list.files(target_folder)
+  powerpoints_to_archive <- lw_powerpoints[str_detect(lw_powerpoints,paste0(xxx,".pptx"))]
+  for(yyy in powerpoints_to_archive){
+    date_check <- yyy %>% str_sub(1, 10) %>% as.Date() # check that they're past lessons!
+    if(date_check < Sys.Date()+9){
+      file.rename(from = paste0(target_folder,"//",yyy),
+                  to = paste0(archive_folder,"//",xxx,"//",yyy))
+    }}}
 
 
 # function for finding date of any given next weekday
@@ -31,25 +68,7 @@ nextweekday <- function(date, wday) {
 }
 
 
-lesson_template_file_location <- paste0(lesson_template_folder, "//", lesson_template_name)
-
-
-
-### move last week's pptx into archive
-
-for(xxx in unique(timetable[,4])){
-  lw_powerpoints <- list.files(target_folder)
-  powerpoints_to_archive <- lw_powerpoints[str_detect(lw_powerpoints,paste0(xxx,".pptx"))]
-  for(yyy in powerpoints_to_archive){
-    date_check <- yyy %>% str_sub(1, 10) %>% as.Date() # check that they're past lessons!
-    if(date_check < Sys.Date()){
-      file.rename(from = paste0(target_folder,"//",yyy),
-                  to = paste0(archive_folder,"//",xxx,"//",yyy))
-    }}}
-
-
-
-### create this week's pptx files
+# create this week's pptx files
 
 for(xxx in 1:nrow(timetable)){
   file.copy(lesson_template_file_location, target_folder)
